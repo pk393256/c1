0526961e05d88148a64272e86454d75b28900de2
pk393256 <48841782+pk393256@users.noreply.github.com>
2022-04-25 23:09:40 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,16 @@
-fetch(`https://pokeapi.co/api/v2/pokemon/`).then((response) => {
-/* console.log(response.status) */
-  response.json().then((result) => {
-    console.log(result)
-    return result
-    
-    
-  })
-})
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p id="data">
+
+    </p>
+</body>
+<script href="./code.js"></script>
+
+</html>
